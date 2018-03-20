1828618e8602ceafe39e0a26ac1925fc41745363
Andre Neiva <agpna@iscte-iul.pt>
2018-03-20 10:17:48 +0000

    Created Worker class to be used as engine. Commented wainting for parts.

---
+++ src/Engine/Worker.java
@@ -0,0 +1,48 @@
+package Engine;
+
+import java.util.ArrayList;
+
+public class Worker {
+
+	private Mail mail_sender;
+//	private GUI frame;
+//	private ArrayList<Variavel> variables = new ArrayList<Variavel>();
+	//private Xml file = new Xml();
+//	private User user;
+	public Worker(){
+		//LogIn login = new LogIn(this);
+	}
+	
+	public void Lauch(){
+		//frame = new GUI(this);
+		//frame.setVisible(true);
+		
+	}
+	public static void main(String[] args) {
+		new Worker();
+	}
+	
+//	public void setUser(User u){
+//		user=u;
+//	}
+	
+	public void sendMail(String from, String to, String subject, String body){
+		mail_sender = new Mail();
+		mail_sender.setValues(from, to, subject, body);
+		mail_sender.start();
+	}
+	
+//	public void addVariable(Variavel v){
+//		variables.add(v);
+//	}
+	
+	public void saveProblem(){
+//		file.initiate(frame.getEmail(), frame.getProblem(), frame.getDescription(), frame.getDate());
+//		for(Variavel var: variables){
+//		}
+	}
+
+//	public User getUser() {
+//		return user;
+//	}
+}
